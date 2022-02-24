a1c0b31ef127deedb68d5c88f8e3e2ae484a2358
Arun Mahendra <arun.mahendra@shopify.com>
2022-02-24 13:52:53 -0500

    added option to enable open metrics format

--- exporter/prometheusexporter/config.go
+++ exporter/prometheusexporter/config.go
@@ -44,6 +44,9 @@ type Config struct {
 
 	// ResourceToTelemetrySettings defines configuration for converting resource attributes to metric labels.
 	ResourceToTelemetrySettings resourcetotelemetry.Settings `mapstructure:"resource_to_telemetry_conversion"`
+
+	//enables OpenMetrics encoding option for prometheus exporter.
+	EnableOpenMetrics bool `mapstructure:"enable_open_metrics"`
 }
 
 var _ config.Exporter = (*Config)(nil)
