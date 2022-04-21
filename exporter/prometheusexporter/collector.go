7a734a60f33e961bbf6862fbcf9280b240179e30
Tanner Bruce <tanner.bruce@shopify.com>
2022-04-21 13:43:56 -0500

    prometheusexporter: add exemplar support

--- exporter/prometheusexporter/collector.go
+++ exporter/prometheusexporter/collector.go
@@ -194,6 +194,7 @@ func (c *collector) convertDoubleHistogram(metric pmetric.Metric, resourceAttrs 
 
 	indicesMap := make(map[float64]int)
 	buckets := make([]float64, 0, len(ip.MBucketCounts()))
+
 	for index, bucket := range ip.MExplicitBounds() {
 		if _, added := indicesMap[bucket]; !added {
 			indicesMap[bucket] = index
@@ -215,9 +216,34 @@ func (c *collector) convertDoubleHistogram(metric pmetric.Metric, resourceAttrs 
 		points[bucket] = cumCount
 	}
 
+	arrLen := ip.Exemplars().Len()
+	exemplars := make([]prometheus.Exemplar, arrLen)
+	for i := 0; i < arrLen; i++ {
+		e := ip.Exemplars().At(i)
+
+		labels := make(prometheus.Labels, e.FilteredAttributes().Len())
+		e.FilteredAttributes().Range(func(k string, v pdata.Value) bool {
+			labels[k] = v.AsString()
+			return true
+		})
+
+		exemplars[i] = prometheus.Exemplar{
+			Value:     e.DoubleVal(),
+			Labels:    labels,
+			Timestamp: e.Timestamp().AsTime(),
+		}
+	}
+
 	m, err := prometheus.NewConstHistogram(desc, ip.Count(), ip.Sum(), points, attributes...)
 	if err != nil {
 		return nil, err
+	}
+
+	if arrLen > 0 {
+		m, err = prometheus.NewMetricWithExemplars(m, exemplars...)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	if c.sendTimestamps {
