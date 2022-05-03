c7db1ff8639dd99454077cd2f2426332499dba02
Tanner Bruce <tanner.bruce@shopify.com>
2022-05-03 16:44:44 -0500

    prometheusexporter: use attributevalue

--- exporter/prometheusexporter/collector.go
+++ exporter/prometheusexporter/collector.go
@@ -221,7 +221,7 @@ func (c *collector) convertDoubleHistogram(metric pmetric.Metric, resourceAttrs 
 		e := ip.Exemplars().At(i)
 
 		labels := make(prometheus.Labels, e.FilteredAttributes().Len())
-		e.FilteredAttributes().Range(func(k string, v pdata.Value) bool {
+		e.FilteredAttributes().Range(func(k string, v pdata.AttributeValue) bool {
 			labels[k] = v.AsString()
 			return true
 		})
